defc362da3cf5eff9aab98113877387d53055b7f
alexanderpatcar1234-ctrl <alexanderpatcar1234@gmail.com>
2026-09-07 11:35:36 -0500

    Add files via upload

---
+++ Semana6.py
@@ -0,0 +1,23 @@
+# Comó asignar un nuevo valor a una variable ya existente
+var = 1
+print("Variables con un valor primario: ",var)
+var = "ALEXANDER"
+print("Variable con un nuevo valor asignado: ",var)
+var = 100
+var = 200 + 300
+print(var)
+
+# LAB
+
+David = 3 
+Madison = 5
+Alex = 6
+print("Galletita tiene: ", David, "manzanas --->","Madison tiene: ", Madison, "manzanas --->","Alex tiene: ", Alex, "manzanas --->")#TOTAL DE MANZANAS
+Total_de_manzanas= David + Madison + Alex
+print("Total de manzanas que suman los 3 es :", Total_de_manzanas)
+# Operadores Abreviados
+x = 3
+# x = x * 2
+x = x * 2
+print("El resultado es:", x)
+
