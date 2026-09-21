de2972106b4d0cbd1b8d8e0e1327033b1a37f5e0
alexanderpatcar1234-ctrl <alexanderpatcar1234@gmail.com>
2026-09-21 11:31:46 -0500

    Add files via upload

---
+++ Operadores_Cadena.py
@@ -0,0 +1,17 @@
+# print("Cadena 1 "+ "Cadena 2")
+# Nom1 = "Rodrigo"
+# Nom2 = "Juan David"
+# print(Nom1 +" y " + Nom2 + ": son pepes")
+
+# Nombre = input("¿Me puedes dar tu nombre por favor? ")
+# Apellido = input("¿Me puedes dar tu apellido por favor? ")
+# print("Gracias. ")
+# print("\nTu nombre es " + Nombre + " " + Apellido + ".")
+
+print("James"*3)
+print(3 * "an")
+print(5 * "2")
+
+print("+" + 40 * "😑" + "+")
+print(("😑" + " " * 40 + "😑\n") * 20, end="")
+print("+" + 40 * "😑" + "+")
