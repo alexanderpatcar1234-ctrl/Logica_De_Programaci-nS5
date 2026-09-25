4cb2dcd614c506f8f0e8a7a23e2008c4b6e9ecc1
alexanderpatcar1234-ctrl <alexanderpatcar1234@gmail.com>
2026-09-25 12:07:11 -0500

    Add files via upload

---
+++ Practica2.py
@@ -0,0 +1,4 @@
+Nombre = input("¿Me puedes dar tu nombre por favor?:")
+Apellido = input("Me puedes dar tu apellido por favor?:")
+print("Gracias.\nTu nombre y apellido es: " + Nombre + " " + Apellido)
+
